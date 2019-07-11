685455e944e84f9390966a000284cc1764a958b8
prrrcl <adrianporcelnavarro@hotmail.com>
2019-07-11 14:41:41 +0200

    Edited some styles

--- components/Loading.js
+++ components/Loading.js
@@ -4,16 +4,31 @@ function Loading(parent) {
 
   this.parent = parent;
   this.elements = null;
-
+  this.dots = ''
 }
 
 Loading.prototype.generate = function(){
+  
   this.elements = `
   <div class="loading">
-    <p>Cargando...</p>
+    <p>Cargando</p>
+    <p class="dots"></p>
   </div>
   `;
   this.render();
+  this.etc();
+}
+Loading.prototype.etc = function(){
+  var dotsDOM = document.querySelector('.dots');
+  setInterval(()=>{
+    
+    this.dots += '.'
+    if(this.dots === '....'){
+      this.dots = '';
+    }
+    dotsDOM.innerHTML = this.dots;
+  },200);
+  
 }
 Loading.prototype.render = function (){
   this.parent.innerHTML = this.elements;
