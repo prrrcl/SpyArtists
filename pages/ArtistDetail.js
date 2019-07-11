eee572ff79597824090d8f566b30ea53dd6fcf85
prrrcl <adrianporcelnavarro@hotmail.com>
2019-07-11 15:56:00 +0200

    Added some styles

--- pages/ArtistDetail.js
+++ pages/ArtistDetail.js
@@ -11,6 +11,7 @@ function ArtistDetail(parent, artist) {
 }
 
 ArtistDetail.prototype.generate = async function () {
+  var imagesElements = [];
   this.loading = new Loading(this.parent);
   this.loading.generate();
   await this.connectToAPI();
@@ -19,11 +20,14 @@ ArtistDetail.prototype.generate = async function () {
       <h3>Albums</h3>
       <section class="cards-container">
       `
-  this.albums.forEach( (album, index) =>{
+  this.albums.forEach( (album, index) => {
+    var img = document.createElement('img');
+    img.setAttribute('src', album.cover.images[0].image)
+    imagesElements.push(img);
     this.elements += `
     <article class="card-album card-album-${(index+1)}">
       <h4>${album.album.album_name}</h4>
-      <img src="${album.cover.images[0].image}">
+      <div style="width:10%;height:10%" class="image-loading lds-eclipse"><div>
     </article>
     `;
   })
@@ -33,6 +37,22 @@ ArtistDetail.prototype.generate = async function () {
     </section>
   `
   this.render();
+  var article = document.querySelectorAll('.card-album');
+  var loading = document.querySelectorAll('.image-loading');
+
+  imagesElements.forEach((image, index) => {
+    image.onload = function() {
+      article[index].appendChild(image);
+      loading[index].remove();
+    }
+  })
+
+  
+
+  // setTimeout(() => {
+  // }, 3000);
+
+
 }
 ArtistDetail.prototype.render = function () {
   this.parent.innerHTML = this.elements;
