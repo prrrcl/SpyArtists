77a86dda2eec2c1abd00dbef4d9afe4da986d9f6
prrrcl <adrianporcelnavarro@hotmail.com>
2019-07-15 12:13:44 +0200

    Refactor Router.js

--- Router.js
+++ Router.js
@@ -1,35 +1,36 @@
 'use strict';
 
-function Router() {
-  this.page = null;
-}
 
-Router.prototype.buildDOM = function (url, parent, artist) {
-  switch (url) {
-    case '/':
-      this.generateLandingPage(parent);
-      break;
-    case '/artistslist':
-      this.generateListArtistsPage(parent, artist);
-      break;
-    case '/viewartist':
-      this.generateViewArtistPage(parent, artist);
-      break;
-    default:
-      this.generateLandingPage(parent);
+class Router{
+  constructor(){
+    this.page = null
+  }
+  buildDOM(url, parent, artist){
+    switch (url) {
+      case '/':
+        this.generateLandingPage(parent);
+        break;
+      case '/artistslist':
+        this.generateListArtistsPage(parent, artist);
+        break;
+      case '/viewartist':
+        this.generateViewArtistPage(parent, artist);
+        break;
+      default:
+        this.generateLandingPage(parent);
+    }
+  }
+  generateLandingPage(parent){
+    this.page = new LandingPage(parent);
+    this.page.generate();
+  }
+  generateListArtistsPage(parent, artist){
+    this.page = new ListOfArtists(parent, artist);
+    this.page.generate();
+  }
+  generateViewArtistPage(parent, artist){
+    this.page = new ArtistDetail(parent, artist);
+    this.page.generate();
   }
 }
-Router.prototype.generateLandingPage = function (parent){
-  this.page = new LandingPage(parent);
-  this.page.generate();
-}
-Router.prototype.generateListArtistsPage = function(parent, artist){
-  this.page = new ListOfArtists(parent, artist);
-  this.page.generate();
-}
-Router.prototype.generateViewArtistPage = function(parent, artist){
-  this.page = new ArtistDetail(parent, artist);
-  this.page.generate();
-}
-
-let routerInstance = new Router();
+const routerInstance = new Router();
