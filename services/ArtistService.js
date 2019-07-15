9857249162265b9227ca8884e49c3db4f2f519b0
prrrcl <adrianporcelnavarro@hotmail.com>
2019-07-15 11:55:23 +0200

    Refactor ArtistService

--- services/ArtistService.js
+++ services/ArtistService.js
@@ -1,27 +1,28 @@
 'use strict';
-function ArtistService(){
-  this.baseURL = 'https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/';
-  this.formatAndApiKey = '&format=json&apikey=41d657034f23a97207f58cde2844b01a';
-  this.baseURLWiki = 'https://es.wikipedia.org/w/api.php?action=query&list=search&srprop=snippet&format=json&origin=*&utf8=&srsearch=';
+class ArtistService{
+  constructor(){
+    this.baseURL = 'https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/';
+    this.formatAndApiKey = '&format=json&apikey=41d657034f23a97207f58cde2844b01a';
+    this.baseURLWiki = 'https://es.wikipedia.org/w/api.php?action=query&list=search&srprop=snippet&format=json&origin=*&utf8=&srsearch=';
+  }
+  async getArtists(artist){
+    const response = await fetch(`${this.baseURL}artist.search?q_artist=${artist}&page=1&page_size=5${this.formatAndApiKey}`);
+    const data = await response.json();
+    const arrayOfArtits = data.message.body.artist_list;
+    
+    const artitsWithInfo = await Promise.all(arrayOfArtits.map(async (artist)=>{
+      const name = artist.artist.artist_name;
+      const artistInfo = await this.getDetailsArtis(name);
+      artist.info = artistInfo;
+      return artist;
+    }))
+    return artitsWithInfo;
+  }
+  async getDetailsArtis(artist){
+    const response = await fetch(`${this.baseURLWiki}${artist}`);
+    const data = await response.json();
+    return data;
+  }
 }
-ArtistService.prototype.getArtists = async function (artist){
-  let response = await fetch(`${this.baseURL}artist.search?q_artist=${artist}&page=1&page_size=5${this.formatAndApiKey}`);
-  let data = await response.json();
-  let arrayOfArtits = data.message.body.artist_list;
 
-  let artitsWithInfo = await Promise.all(arrayOfArtits.map(async (artist)=>{
-    let name = artist.artist.artist_name;
-    let artistInfo = await this.getDetailsArtis(name);
-    artist.info = artistInfo;
-    return artist;
-  }))
-  console.log(artitsWithInfo)
-  return artitsWithInfo;
-}
-ArtistService.prototype.getDetailsArtis = async function (artist){
-  let response = await fetch(`${this.baseURLWiki}${artist}`);
-  let data = await response.json();
-  return data;
-}
-
-let artistServiceInstance = new ArtistService();
+const artistServiceInstance = new ArtistService();
